4cbf96080057ab2396f4e39b9782d32133110125
Daniel Gonzalez <gdaniel387@gmail.com>
2017-04-11 12:25:00 -0400

    Update Login.java

--- Login.java
+++ Login.java
@@ -2,11 +2,16 @@ package system;
 import java.util.Scanner;
 public class Login {
 	
-	Scanner input = new Scanner (System.in);
-	
-	System.out.println("Enter username");
-	
-	
-	System.out.println("Enter password");
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter username");
+		String username = input.next();
+
+		System.out.println("Enter password");
+		String password = input.next();
+
+	}
 
 }
