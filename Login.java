aff5541456f84f8d798c71ca0c76486579e02f7d
Daniel Gonzalez <gdaniel387@gmail.com>
2017-04-11 12:34:10 -0400

    Update Login.java

--- Login.java
+++ Login.java
@@ -5,12 +5,16 @@ public class Login {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
+		User u1 = new User();
 
 		System.out.println("Enter username");
 		String username = input.next();
+		if (username == u1.getUsername())
+		
 
 		System.out.println("Enter password");
 		String password = input.next();
+		if (password == u1.getPassword());
 
 	}
 
