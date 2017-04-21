cf78e3a8be4d03b29d90974765a6362566556297
Daniel Gonzalez <gdaniel387@gmail.com>
2017-04-20 22:31:34 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,9 +1,9 @@
 package system;
 
 public class User {
-	
-	//data fields
-	
+
+	// data fields
+
 	private String firstName;
 	private String lastName;
 	private String address;
@@ -14,72 +14,104 @@ public class User {
 	private String email;
 	private int ssn;
 	private String securityQuestion;
+	private String securityAnswer;
 	
-	// Constructors
+	//Constructors
+	
+	public User () {
+		
+	}
 	
 	public User (String username) {
 		this.username = username;
 	}
-	
-	//Setters and getters
-	
+
+	// Setters and getters
+
+	public String getSecurityAnswer() {
+		return securityAnswer;
+	}
+
+	public void setSecurityAnswer(String securityAnswer) {
+		this.securityAnswer = securityAnswer;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public int getZipCode() {
 		return zipCode;
 	}
+
 	public void setZipCode(int zipCode) {
 		this.zipCode = zipCode;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public int getSsn() {
 		return ssn;
 	}
+
 	public void setSsn(int ssn) {
 		this.ssn = ssn;
 	}
+
 	public String getSecurityQuestion() {
 		return securityQuestion;
 	}
+
 	public void setSecurityQuestion(String securityQuestion) {
 		this.securityQuestion = securityQuestion;
 	}
