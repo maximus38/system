e8f7dcc43d0e771555cbfd43c309117ea50ce124
abandali2 <abandali2@student.gsu.edu>
2017-04-24 14:46:00 -0400

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -2,4 +2,36 @@ package system;
 
 public class Admin extends User {
 
+   private String addFlight;
+	
+	 private String deleteFlight;
+	
+	Admin(){
+		
+		addFlight=null;
+		
+		deleteFlight = null;
+		
+	}
+	
+	Admin(String NewAddFlight, String NewDeleteFlight) {
+		
+		addFlight = NewAddFlight;
+		
+		deleteFlight= NewDeleteFlight;
+	}
+	
+	void setAddFlight(String NewAddFlight) {
+		
+		addFlight = NewAddFlight;
+		
+		
+	}
+	
+	void setDeleteFlight(String NewDeleteFlight) {
+		
+		deleteFlight = NewDeleteFlight;
+		
+		
+	}
 }
