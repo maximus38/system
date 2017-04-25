0e7730ef2096a483860ebf6b94e6247372fec8fc
Daniel Gonzalez <gdaniel387@gmail.com>
2017-04-25 13:05:43 -0400

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,25 +1,22 @@
 package system;
+
 import java.util.Scanner;
+
 public class MainMenu {
-	
-	public static void main (String [] args) {
+
+	public static void main(String[] args) {
 		
-		Scanner input = new Scanner (System.in);
+		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Enter email");
-		String email = input.next();
+		System.out.println("Dont have an account? Create an account by pressing 1");
 		
-		System.out.println("Enter username");
-		String username = input.next();
-		
-		System.out.println("Enter password");
-		String password = input.next();
-		
-		System.out.println("Don't have an account? Create one here");
-		
-		
-		
-		
+		System.out.println("If you have an account press 2 to enter login info");
+		if (input.nextInt() == 1) 
+			Registration.main(args);
+			else if (input.nextInt() == 2) {
+				Login.main(args);
+			}
+
 	}
 
 }
